6cca5e3bdb4c65d8fd812afef7b8db93739640b4
rikiTada <riki.tada777@gmail.com>
2024-06-07 14:46:54 +0900

    fix: install.(bat ⇒ sh)

---
+++ install.sh
@@ -0,0 +1,75 @@
+# bash
+
+# vscode settings & theme
+code --install-extension ms-ceintl.vscode-language-pack-ja
+code --install-extension pkief.material-icon-theme
+code --install-extension ixkaito.ktrz-monokai
+
+# Git
+code --install-extension GitHub.vscode-pull-request-github
+code --install-extension GitHub.codespaces
+code --install-extension GitHub.vscode-github-actions
+code --install-extension donjayamanne.githistory
+code --install-extension mhutchie.git-graph
+code --install-extension eamodio.gitlens
+
+# esLint
+code --install-extension dbaeumer.vscode-eslint
+
+# Docker
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-vscode-remote.remote-containers
+
+# React
+code --install-extension nino.easy-simple-react-snippets-lite
+
+# Tailwind
+code --install-extension bradlc.vscode-tailwindcss
+
+# API
+code --install-extension postman.postman-for-vscode
+
+# markdown
+code --install-extension davidanson.vscode-markdownlint
+code --install-extension yzhang.markdown-all-in-one
+
+# prettier
+code --install-extension esbenp.prettier-vscode
+code --install-extension yoavbls.pretty-ts-errors
+
+# snippets
+code --install-extension akamud.vscode-javascript-snippet-pack
+code --install-extension xabikos.JavaScriptSnippets
+
+# ======== Recommended ========
+code --install-extension adpyke.codesnap
+code --install-extension prisma.prisma
+code --install-extension ms-vscode.vscode-typescript-next
+code --install-extension sleistner.vscode-fileutils # craete new file
+code --install-extension oderwat.indent-rainbow
+code --install-extension Arjun.swagger-viewer
+code --install-extension Gruntfuggly.todo-tree
+code --install-extension bradgashler.htmltagwrap
+code --install-extension usernamehw.errorlens
+code --install-extension vincaslt.highlight-matching-tag
+code --install-extension formulahendry.auto-rename-tag
+code --install-extension ionutvmi.path-autocomplete
+code --install-extension simonsiefke.svg-preview
+code --install-extension kisstkondoros.vscode-gutter-preview #img preview
+
+
+# ======== If necessary ========
+code --install-extension suhelmakkad.shadcn-ui # shadcn-ui
+# code --install-extension WallabyJs.console-ninja
+# code --install-extension leodevbro.blockman
+# code --install-extension pranaygp.vscode-css-peek
+# code --install-extension streetsidesoftware.code-spell-checker
+# code --install-extension ritwickdey.LiveServer
+
+# ======== others ========
+# code --install-extension pmneo.tsimporter
+# code --install-extension mrmlnc.vscode-scss
+# code --install-extension ryu1kn.partial-diff
+# code --install-extension glenn2223.live-sass
+# code --install-extension ms-vscode.live-server
+# code --install-extension donjayamanne.jquerysnippets
